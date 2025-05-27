1aa0d6af1848553d978632f6645d540fb4deb8ad
Paula <Lepezmpaula@gmail.com>
2025-05-27 12:39:51 -0300

    Funcion add + prompt para lista

--- js/simuladordecarrito.js
+++ js/simuladordecarrito.js
@@ -15,4 +15,27 @@ const productos = [
 // carrito de compras
 let carrito = [];
 
+//menu de opciones
 
+function mostrarproductos () {
+  let lista = "productos disponibles:/n";
+ //for each por cada producto + funcion flecha retorna el prod.
+  productos.forEach((producto) => {
+    lista += `ID: ${producto.id}, Nombre: ${producto.nombre}, Precio: $${producto.precio}\n`;
+  });
+  alert(lista);
+}
+
+
+//funcion agragar al carrito con un Prompt
+function agregarAlCarrito() {
+  const idProducto = parseInt(prompt("Ingrese el ID del producto que desea agregar al carrito:"));
+  const producto = productos.find((p) => p.id === idProducto);
+  
+  if (producto) {
+    carrito.push(producto);
+    alert(`Producto agregado: ${producto.nombre}`);
+  } else {
+    alert("Producto no encontrado.");
+  }
+}
