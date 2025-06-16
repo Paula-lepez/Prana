661bada7e40b00de7ae7d55ab4430837809eecb3
Paula <Lepezmpaula@gmail.com>
2025-06-16 16:06:38 -0300

    Agregar proximamente en Json las variables

--- js/datosjason.js
+++ js/datosjason.js
@@ -1,2 +1,2 @@
-    //PARA VER EN CONSOLA TIPEAR JSON.parse(localStorage.getItem("carrito"))   
-
+    //PARA VER EN CONSOLA TIPEAR -->JSON.parse(localStorage.getItem("carrito"))   
+//    PROXIMO PASO   : Agregar a Json los producctos para despues hacer FETCH y mostrar en el HTML
